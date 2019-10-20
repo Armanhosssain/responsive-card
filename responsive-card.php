205b8426e92ceeb557cd9c98df5e8e22f855eb92
Arman hossain <armanh688@gmail.com>
2019-10-20 23:59:44 +0600

    responsive card

---
+++ responsive-card.php
@@ -0,0 +1,145 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="css/uikit.min.css">
+  <link rel="shortcut icon" type="image/x-icon" href="img/a.png">
+  <link rel="stylesheet" href="css/font-awesome.min.css">
+  <link rel="stylesheet" href="css/bootstrap.css">
+  <link rel="stylesheet" href="css/style.css">
+	<title>responsive card</title>
+</head>
+<body>
+	<!-- start price table -->
+<section id="price-table" class="py-5">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-4 my-3">
+        <div class="card text-center">
+          <div class="card-header">
+            <h3>Service Plan One</h3>
+          </div>
+          <div class="card-body">
+            <div class="card-title">
+            <h3>$59.99/Month</h3>
+            </div>
+            <div class="card-text mb-3 lead">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            </div>
+            <ul class="list-group">
+              <li class="list-group-item">
+                <i class='fa fa-check'></i>Feature One
+              </li>
+              <li class="list-group-item">
+                <i class='fa fa-check'></i>Feature Two
+              </li>
+              <li class="list-group-item">
+                <i class='fa fa-check'></i>Feature Three
+              </li>
+              <li class="list-group-item">
+                <i class='fa fa-check'></i>Feature Four
+              </li>
+              <li class="list-group-item">
+                <i class='fa fa-check'></i>Feature Five
+              </li>
+            </ul>
+            <a href="#" class="btn btn-block btn-primary mt-2">Order Now</a>
+          </div>
+          <div class="card-footer">
+            <p class="muted mb-0">
+              1 Year Plan
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4 my-3">
+        <div class="card text-center">
+          <div class="card-header">
+            <h3>Service Plan Two</h3>
+          </div>
+          <div class="card-body">
+            <div class="card-title">
+            <h3>$59.99/Month</h3>
+            </div>
+            <div class="card-text mb-3 lead">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            </div>
+            <ul class="list-group">
+              <li class="list-group-item">
+                <i class='fa fa-check'></i>Feature One
+              </li>
+              <li class="list-group-item">
+                <i class='fa fa-check'></i>Feature Two
+              </li>
+              <li class="list-group-item">
+                <i class='fa fa-check'></i>Feature Three
+              </li>
+              <li class="list-group-item">
+                <i class='fa fa-check'></i>Feature Four
+              </li>
+              <li class="list-group-item">
+                <i class='fa fa-check'></i>Feature Five
+              </li>
+            </ul>
+            <a href="#" class="btn btn-block btn-primary mt-2">Order Now</a>
+          </div>
+          <div class="card-footer">
+            <p class="muted mb-0">
+              1 Year Plan
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4 my-3">
+        <div class="card text-center">
+          <div class="card-header">
+            <h3>Service Plan Three</h3>
+          </div>
+          <div class="card-body">
+            <div class="card-title">
+            <h3>$59.99/Month</h3>
+            </div>
+            <div class="card-text mb-3 lead">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            </div>
+            <ul class="list-group">
+              <li class="list-group-item">
+                <i class='fa fa-check'></i>Feature One
+              </li>
+              <li class="list-group-item">
+                <i class='fa fa-check'></i>Feature Two
+              </li>
+              <li class="list-group-item">
+                <i class='fa fa-check'></i>Feature Three
+              </li>
+              <li class="list-group-item">
+                <i class='fa fa-check'></i>Feature Four
+              </li>
+              <li class="list-group-item">
+                <i class='fa fa-check'></i>Feature Five
+              </li>
+            </ul>
+            <a href="#" class="btn btn-block btn-primary mt-2">Order Now</a>
+          </div>
+          <div class="card-footer">
+            <p class="muted mb-0">
+              1 Year Plan
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<!-- end price table -->
+  <script src="js/jquery.min.js"></script>
+  <script src="js/popper.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/uikit.min.js"></script>
+  <script src="js/uikit-icons.min.js"></script>
+  <script src="js/slick.min.js"></script>
+  <script src="js/main.js"></script>
+</body>
+</html>
